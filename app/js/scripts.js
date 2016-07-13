67450a888ec62f46d8c03a639861931aa09c49cf
Roman Pylypenko <roman@globalit.no>
2016-07-13 19:24:36 +0300

    add files

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,26 +1,32 @@
 $(document).ready(function(){
-	$("#mobile-nav-btn").click( function(){
-		$(this).parent(".header").find(".nav").toggleClass("nav_open");
+	$('.slider').slick({
+		slidesToScroll: 1,
+		autoplay: false,
+		autoplaySpeed: 4000,
+		dots: true
 	});
-
-var $menu = $(".nav");
-var width
-var height
-window.onresize = function () {
-    width = document.documentElement.clientWidth;
-    height = document.documentElement.clientHeight;        
-}
-
-	$(window).scroll(function(){
-		var height = document.documentElement.clientHeight;
-		if ( width > 939 ){
-			if ( $(this).scrollTop() > 340 ){
-				$menu.addClass("nav_moved");
-			} else if($(this).scrollTop() <= height ){
-				$menu.removeClass("nav_moved");
-			}
-		} else{
-			$menu.removeClass("nav_moved");
-		}
-	});
+//	$("#mobile-nav-btn").click( function(){
+//		$(this).parent(".header").find(".nav").toggleClass("nav_open");
+//	});
+//
+//var $menu = $(".nav");
+//var width
+//var height
+//window.onresize = function () {
+//    width = document.documentElement.clientWidth;
+//    height = document.documentElement.clientHeight;
+//}
+//
+//	$(window).scroll(function(){
+//		var height = document.documentElement.clientHeight;
+//		if ( width > 939 ){
+//			if ( $(this).scrollTop() > 340 ){
+//				$menu.addClass("nav_moved");
+//			} else if($(this).scrollTop() <= height ){
+//				$menu.removeClass("nav_moved");
+//			}
+//		} else{
+//			$menu.removeClass("nav_moved");
+//		}
+//	});
 });
